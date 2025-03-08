d7479ba95d2a20aeba51c5a9c5904547cd99ed10
omarazhar10 <omar10.azhar@gmail.com>
2025-03-08 20:51:59 +0500

    L1 python

---
+++ first-project.py
@@ -0,0 +1,9 @@
+name = "Omar"
+age = "12"
+skills = "coding, no maths"
+studies = "HTML, CSS, JavaScript & Python"
+
+print(f"Hi, my name is {name}")
+print(f"Hi, my age is {age}")
+print(f"Hi, my skills are {skills}")
+print(f"Hi, I am studiying {studies}")
